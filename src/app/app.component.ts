0a6c22980bdd2d2aa8b39d04eebf3b2009c82ccc
Zakaria Amarifi <z.amarifi@dm73.net>
2017-06-23 23:58:17 +0000

    change names for fun

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,16 @@ export class Hero {
 }
 
 const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
+    { id: 11, name: 'Lord Buckethead' },
+    { id: 12, name: 'Kaiser Sosee' },
+    { id: 13, name: 'Donnie Darko' },
+    { id: 14, name: 'Mr. Bombastic' },
+    { id: 15, name: 'Lady Vengeance' },
+    { id: 16, name: 'Dr Dre' },
+    { id: 17, name: 'Machete' },
+    { id: 18, name: 'Alfred' },
+    { id: 19, name: 'Looooool' },
+    { id: 20, name: 'WTF' }
 ];
 
 @Component({
