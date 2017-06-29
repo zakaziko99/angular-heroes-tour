c2876a5414404752827d0693c89278ff89dbf9b5
Zakaria Amarifi <z.amarifi@dm73.net>
2017-06-29 19:42:23 +0000

    separating css and html from script

--- src/app/components/hero-detail/hero-detail.component.ts
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -3,16 +3,7 @@ import { Hero } from '../../classes/hero';
 
 @Component({
     selector: 'hero-detail',
-    template: `
-        <div class="hero-detail-component" *ngIf="hero">
-            <h2>{{ hero.name }} details!</h2>
-            <div><label>id: </label>{{ hero.id }}</div>
-            <div>
-                <label>name: </label>
-                <input [(ngModel)]="hero.name" placeholder="name"/>
-            </div>
-        </div>
-    `,
+    templateUrl: './hero-detail.html',
     styleUrls: ['./hero-detail.css']
 })
 export class HeroDetailComponent {
