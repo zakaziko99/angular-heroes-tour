f0cdb66d3e0861b984943694de4450c80b6e5013
Zakaria Amarifi <z.amarifi@dm73.net>
2017-07-11 23:30:14 +0100

    destructuring code from app to components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { Hero } from './classes/hero';
-import { HeroService } from './services/hero/hero.service';
 
 @Component({
-    selector: 'app-root',
+    selector: 'my-app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
     title = 'Tour of Heroes (Zakaria version)';
-    selectedHero: Hero;
-    heroes: Hero[];
-
-    constructor(private heroService: HeroService) {}
-
-    ngOnInit(): void {
-        this.getHeroes();
-    }
-
-    getHeroes(): void {
-        this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
-    }
-
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 import { HeroService } from './services/hero/hero.service'
 
 @NgModule({
     declarations: [
         AppComponent,
+        HeroesComponent,
         HeroDetailComponent
     ],
     imports: [
